6616c5040574b40452b14428a06b305e70f17493
Michael Crimando <crimandomichael@yahoo.com>
2019-03-31 14:12:25 -0400

    Add files via upload

---
+++ saveFeedback.js
@@ -0,0 +1,11 @@
+<script type ="text/javascript">
+ function WriteToFile(passForm) {
+
+    set fso = CreateObject("Scripting.FileSystemObject");  
+    set s = fso.CreateTextFile("C:\guestBookComments.txt", True);
+    s.writeline(document.passForm.input1.value);
+    s.writeline(document.passForm.input2.value);
+    s.writeline(document.passForm.input3.value);
+    s.Close();
+ }
+  </script>
