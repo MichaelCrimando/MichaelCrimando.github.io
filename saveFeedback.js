ae112c094ba6d66dcac057d7a0377dba501a73c3
Michael Crimando <crimandomichael@yahoo.com>
2019-03-31 17:01:02 -0400

    FINAL FINAL

--- saveFeedback.js
+++ saveFeedback.js
@@ -1,12 +1,14 @@
 
 
-function clearFields(){
+function sendFeedback(){
     document.getElementById("thanks").innerHTML = "<h3>Thanks for your feedback! It has been submitted to a printer that prints it into the trash.</h3><img src=\"/Images/dancingMinion.gif\" alt=\"Under construction animated\ class=\"dancingMinion\"></p>";
 
     document.getElementById("submitButton").disabled = true;
+    document.getElementById("feedbackSent").play();
 }
 function enableSubmit(){
     document.getElementById("submitButton").disabled = false;
+    document.getElementById("radioButtonSound").play();
 }
 //  function WriteToFile(passForm) {
 
