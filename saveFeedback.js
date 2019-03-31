fc1eb7ee5417ae2065677f6e64017a3a699dcc24
Michael Crimando <crimandomichael@yahoo.com>
2019-03-31 14:47:04 -0400

    Add files via upload

--- saveFeedback.js
+++ saveFeedback.js
@@ -2,6 +2,9 @@
 
 function clearFields(){
     console.log("Swaggeroonie");
+    document.getElementById("thanks").innerHTML = "<h3>Thanks for your feedback! It has been submitted to a printer that prints it into the trash.</h3><img src=\"/Images/dancingMinion.gif\" alt=\"Under construction animated\"></p>";
+
+    document.getElementById("submitButton").disabled = true;
 }
 //  function WriteToFile(passForm) {
 
